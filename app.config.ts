4a28cabab8b0d06696340becc5408112d853e052
Evan Jacobs <x@quantizor.dev>
2024-10-25 02:25:09 -0400

    fix resolution

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from '@solidjs/start/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({ vite: { plugins: [tsConfigPaths()] }, ssr: false });
+export default defineConfig({ vite: { plugins: [tsConfigPaths()] }, server: { preset: 'github-pages' }, ssr: false });
